c37c6c760505321ae2ed84b110ea9dca35f04f57
geowalrus4gh <33422391+geowalrus4gh@users.noreply.github.com>
2023-12-19 21:40:47 +0530

    added

--- src/main/java/com/thearaseng/httpecho/controller/EchoController.java
+++ src/main/java/com/thearaseng/httpecho/controller/EchoController.java
@@ -26,6 +26,9 @@ public class EchoController {
 	public String many() {
 		return "Am a many";
 	}
-
+	@GetMapping("/manyAnotherAnother")
+	public String manyAnotherAnother() {
+		return "Am a many";
+	}
 
 }
